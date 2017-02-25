bed81227f221c4bc95a390fe14f89e3d4af914e3
Amr <a.jnumb1@gmail.com>
2017-02-25 21:39:28 +0100

    Update BookRegister.java

--- BookRegister.java
+++ BookRegister.java
@@ -6,44 +6,44 @@ public class BookRegister {
 		
 		
 		while(true) {
-			int command = dialog.readInt("Meny" + "\n"
- 					+ "1: Sätt in en ny bok" + "\n"
- 					+ "2: Tag bort böcker" + "\n"
- 					+ "3: Sök titel från författare/del av författare" + "\n"
- 					+ "4: Sök bok från titel" + "\n"
- 					+ "5: Läs titlar från en fil" + "\n"
- 					+ "6: Skriv titlar till en fil" + "\n"
- 					+ "7: Visa böcker sorterade efter författare" + "\n"
- 					+ "8: Visa böcker sorterade efter titel" + "\n");
+			int command = dialog.readInt("Menu" + "\n"
+ 					+ "1: Insert a new book" + "\n"
+ 					+ "2: delete books" + "\n"
+ 					+ "3: Search title from writer / part of the author name" + "\n"
+ 					+ "4: Search book after title" + "\n"
+ 					+ "5: Read titles from a file" + "\n"
+ 					+ "6: Wrtie titles to a file" + "\n"
+ 					+ "7: Show sorted list by author" + "\n"
+ 					+ "8: Show sorted list by title" + "\n");
 			
 			switch(command){
 			case 1:
-				String author = dialog.readString("Skriv namn på författaren");
-				String title = dialog.readString("Skriv titel");
+				String author = dialog.readString("Write the name of the author");
+				String title = dialog.readString("Write titel");
 				Book book = new Book(author, title);
 				register.insert(book);
 				break;
 			case 2:
-				String nameOfAuthor = dialog.readString("Skriv namn på författaren");
+				String nameOfAuthor = dialog.readString("Write the name of the author");
 				Book b = register.remove(nameOfAuthor);
-				dialog.printString(b.getAuthor() + " böcker är nu borttagna");
+				dialog.printString(b.getAuthor() + " books are now deleted");
 				break;
 			case 3:
-				String searchWord = dialog.readString("Skriv sök ord");
+				String searchWord = dialog.readString("Write a keyword");
 				String result = register.searchByAuthor(searchWord);
 				dialog.printString(result);
 				break;
 			case 4:
-				String t = dialog.readString("Skriv önskad titel");
+				String t = dialog.readString("Enter the desired title");
 				String titles = register.searchByTitle(t);
 				dialog.printString(titles);
 				break;
 			case 5:
-				String filename = dialog.readString("Skriv filsnamn");
+				String filename = dialog.readString("Write name of the file");
 				register.readFromFile(filename);
 				break;
 			case 6:
-				String tofile = dialog.readString("Skriv filsnamn");
+				String tofile = dialog.readString("Write name of the file");
 				register.writeToFile(tofile);
 				break;
 			case 7:
